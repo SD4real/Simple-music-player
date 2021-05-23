83b703f503a13446829cd6b2b97ecb8fc6fa491e
SD4real <70353869+SD4real@users.noreply.github.com>
2021-05-22 22:14:14 -0700

    Update Jukebox.java

--- Jukebox.java
+++ Jukebox.java
@@ -1,8 +1,12 @@
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 
 
 public class Jukebox{
 	
-	public static void main(String[] args){
+	public static void main(String[] args)throws IOException, UnsupportedAudioFileException, LineUnavailableException {
+
 
 	// JFrame 
     new MyFrame();
